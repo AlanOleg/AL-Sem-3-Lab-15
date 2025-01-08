2396b21d15871135898156fccf7594ed275011a3
Alan <57073942+AlanOleg@users.noreply.github.com>
2025-01-08 19:33:06 +0300

    Add files via upload

---
+++ Task 2/AL-Sem-3-Lab-15-Task-2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class TextLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public TextLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void Log(string message)
+    {
+        using (StreamWriter writer = new StreamWriter(_filePath, true))
+        {
+            writer.WriteLine($"{DateTime.Now}: {message}");
+        }
+    }
+}
+
+public class JsonLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public JsonLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void Log(string message)
+    {
+        var logEntry = new
+        {
+            Timestamp = DateTime.Now,
+            Message = message
+        };
+
+        var json = JsonConvert.SerializeObject(logEntry, Newtonsoft.Json.Formatting.Indented);
+        using (StreamWriter writer = new StreamWriter(_filePath, true))
+        {
+            writer.WriteLine(json);
+        }
+    }
+}
+
+public class MyLogger
+{
+    private readonly List<ILogger> _loggers;
+
+    public MyLogger(List<ILogger> loggers)
+    {
+        _loggers = loggers;
+    }
+
+    public void Log(string message)
+    {
+        foreach (var logger in _loggers)
+        {
+            logger.Log(message);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var textLogger = new TextLogger("log.txt");
+        var jsonLogger = new JsonLogger("log.json");
+
+        var loggers = new List<ILogger> { textLogger, jsonLogger };
+        var myLogger = new MyLogger(loggers);
+
+        myLogger.Log("This is a log message.");
+        myLogger.Log("This is another log message.");
+
+        Console.WriteLine("Logs have been written.");
+    }
+}
